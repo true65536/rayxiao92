5ba98da4df62fab5ad69a62de0c37a0af11f4a50
rayxiao92 <rui.xiao@tufts.edu>
2013-05-31 01:06:03 +0800

    try click

--- shuazi.js
+++ shuazi.js
@@ -2,5 +2,5 @@ function shua(){
 	mywindow=window.open("http://erji.net/read.php?tid=1470068&page=e&#a");
 	//setTimeout(function(){mywindow.close()},2000);
 	alert();
-	setTimeout(function(){alert(mywindow.write("haha"));}, 2000);
+	setTimeout(function(){alert(mywindow.document.FORM.Submit.click());}, 2000);
 }
