380fa9b88b98dbb0a5e83e1fd604b5ada272a937
rayxiao92 <rui.xiao@tufts.edu>
2013-05-31 01:52:00 +0800

    post button

--- shuazi.js
+++ shuazi.js
@@ -2,5 +2,9 @@ function shua(){
 	mywindow=window.open("http://erji.net/read.php?tid=1470068&page=e&#a");
 	//setTimeout(function(){mywindow.close()},2000);
 	alert();
-	setTimeout(function(){mywindow.document.FORM.Submit.click();}, 2000);
+	//setTimeout(function(){mywindow.document.FORM.Submit.click();}, 2000);
+}
+
+function post(){
+	mywindow.document.FORM.Submit.click();
 }
