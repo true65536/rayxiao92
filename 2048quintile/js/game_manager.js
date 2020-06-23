f06da4c235d71de3ffe7db3019f354db5d2a7c8d
true65536 <66752659+true65536@users.noreply.github.com>
2020-06-23 14:11:22 -0700

    5x5 version

--- 2048quintile/js/game_manager.js
+++ 2048quintile/js/game_manager.js
@@ -141,8 +141,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 65536 tile
+          if (merged.value === 65536) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
